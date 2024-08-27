357c25a03b59a6b554d10b84b21cdfc416789add
Chrstjan <ps5christian@hotmail.com>
2024-08-27 09:43:36 +0200

    Styling!!

--- React Stopwatch/src/App.jsx
+++ React Stopwatch/src/App.jsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 
 import { Button } from "./components/Button/Button";
 import "./App.scss";
@@ -7,44 +7,49 @@ function App() {
   const [isCounting, setIsCounting] = useState(false);
   const [seconds, setSeconds] = useState(0);
 
-
   const handleStartTimer = () => {
-    setIsCounting(true)
-  }
+    setIsCounting(true);
+  };
 
   const handleStopTimer = () => {
     setIsCounting(false);
-  }
+  };
+
+  const handleResetTimer = () => {
+    setSeconds(0);
+    setIsCounting(false);
+  };
 
   useEffect(() => {
-    let time = 0;
+    let time = 1;
     if (isCounting == true) {
-       time = setInterval(() => {
-        setSeconds(prevSeconds => prevSeconds + 1)
+      time = setInterval(() => {
+        setSeconds((prevSeconds) => prevSeconds + 1);
         console.log("Hello");
-        
-      }, 1000)
-    }
-    else {
-      clearInterval(time);
+      }, 1000);
     }
     return () => {
       if (time) {
-        clearInterval(time)
+        clearInterval(time);
       }
-    }
-  }, [isCounting])
+    };
+  }, [isCounting]);
 
   console.log(isCounting);
-  
+
   return (
     <>
+    <div className="stop-watch">
       <h1>React Stopwatch</h1>
-      <p>{seconds}</p>
+      <span className="timer-container">
+        <p>{seconds}s</p>
+      </span>
       <section className="btn-container">
-        <Button action={() => handleStartTimer()} text='Start' type='start'/>
-        <Button action={() => handleStopTimer()} text='Stop' type={'stop'}/>
+        <Button action={() => handleStartTimer()} text="Start" theme="start" />
+        <Button action={() => handleStopTimer()} text="Stop" theme="stop" />
+        <Button action={() => handleResetTimer()} text="Reset" theme="reset" />
       </section>
+    </div>
     </>
   );
 }
